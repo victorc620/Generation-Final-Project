09e298dcbe9925f3332456bfc36ec5c4bb91c5cc
victorchu620 <waiyat620@gmail.com>
2022-01-28 15:02:38 +0000

    Created hash_id with md5 hashing method

--- data_normalization.py
+++ data_normalization.py
@@ -1,13 +1,23 @@
+from base64 import encode
 import pandas as pd
 import numpy as np
+import hashlib
+from pandas.util import hash_pandas_object
 
-df = pd.read_csv('./csv/chesterfield_25-08-2021_09-00-00.csv', names = ["datetime","Location","fullname", "products+price", "total_price","payment_type","card_number"])
+df_original = pd.read_csv('./csv/chesterfield_25-08-2021_09-00-00.csv', names = ["datetime","Location","fullname", "products+price", "total_price","payment_type","card_number"])
 
 def create_hash_id(df_arg):
-    "Generate a hash id based on the original data (before removing any data)"
-    df_arg["id"]=df_arg.astype(str).sum(1).apply(hash)
-    df_arg["id"]=df_arg["id"].astype(int).apply(abs)
+    """
+    Generate a hash id based on the original data (before removing any data)
+    hashing method still waiting to be updated
+    """
+    df_arg["id"]=df_arg.astype(str).sum(1).apply(lambda x:hashlib.md5(x.encode()).hexdigest())
     return df_arg
+
+    # df_arg["id"] = hash_pandas_object(df_arg)
+    # df_arg["id"]=df_arg.astype(str).sum(1).str.encode('utf-8').apply(hashlib.md5)
+    # df_arg["id"]=df_arg.astype(str).sum(1).apply(hash)
+    # df_arg["id"]=df_arg["id"].astype(int).apply(abs)
 
 def drop_column(df_arg, column):
     "Drop any column in the DataFrame"
@@ -20,8 +30,9 @@ def set_index(df_arg,column):
     return df_arg
 
 # Create function to do 1NF on product & price
-def products_price_explode(df_arg, column, split_criteria):
+def products_price_explode(df, column, split_criteria):
     "Perform 1NF explode to products+price where each role contain one product only"
+    df_arg = df.copy()
     df_arg[column] = df_arg[column].map(str)
     df_arg[column] = df_arg[column].str.split(split_criteria)
     df_arg = df_arg.explode(column)
@@ -40,12 +51,17 @@ def add_product_price_colume(df_arg):
 def remove_price_from_products(df_arg):
     df_arg["products+price"] = df_arg["products+price"].map(lambda x:x.rstrip(' -0123456789.'))
     return df_arg
-df = create_hash_id(df)
-df = drop_column(df, "card_number")
-df = drop_column(df, "full_name")
-df = set_index(df, "id")
 
-print(df)
+
+df_transformed = products_price_explode(df_original, "products+price", ",")
+df_transformed = add_product_price_colume(df_transformed)
+df_transformed = create_hash_id(df_transformed)
+df_transformed = drop_column(df_transformed, "card_number")
+df_transformed = drop_column(df_transformed, "fullname")
+df_transformed = set_index(df_transformed, "id")
+
+print(df_original)
+print(df_transformed)
 
 # def seperate_date_time(df_arg):
 #     df_arg["date"] = df_arg["datetime"].str.split(" ").str[0]
@@ -53,7 +69,5 @@ print(df)
 #     df_arg.drop("datetime", inplace=True, axis =1)
 #     return df_arg
 
-df_1nf = products_price_explode(df, "products+price", ",")
-d1_inf = add_product_price_colume(df_1nf)
-# df_1nf = seperate_date_time(df_1nf)
-print(df_1nf)
+# # df_1nf = seperate_date_time(df_1nf)
+# print(df_1nf)
