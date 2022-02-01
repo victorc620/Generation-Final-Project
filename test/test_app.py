18638f932db6ed137e6ffc9f10e40c94385faab7
Ali <ali2255@outlook.com>
2022-02-01 21:17:26 +0000

    all tests passed

--- test/test_app.py
+++ test/test_app.py
@@ -11,7 +11,7 @@ def drop_column(df_arg, column):
     
 
 def test_drop_column():
-    # arrange
+    # assemble
     df = pd.DataFrame({"foo": [1, 2, 3], "bar": ['a', 'b', 'c']}) 
     mock_drop = Mock()
     mock_drop.return_value = pd.DataFrame({"foo": [1, 2, 3]})
@@ -30,7 +30,7 @@ def set_index(df_arg,column):
 
 
 def test_set_index():
-    #arrange 
+    #assemble 
     idx = pd.Index(['a', 'b', 'c'], name='bar')
     df = pd.DataFrame({"foo": [1, 2, 3], "bar": ['a', 'b', 'c']}) 
     expected = pd.DataFrame({"foo": [1, 2, 3]}, index=idx)
@@ -49,7 +49,7 @@ def products_price_explode(df_arg, column, split_criteria):
 
 
 def test_product_price_explode():
-    # Assemble
+    # assemble
     data = {'col1':[1,1,2,2],'col2':["a","b","c","d"]}
     d = {'col1':[1,2], 'col2':["a,b","c,d"]}
     df = pd.DataFrame(data=d)
@@ -57,10 +57,57 @@ def test_product_price_explode():
     idx = pd.Index([0, 0, 1, 1])
     mock_product_price_explode.return_value = pd.DataFrame(data=data, index=idx)
     expected = mock_product_price_explode()
-    # Act
+    # act
     result = products_price_explode(df, 'col2', ',')
     print(result)
 
-    #Assert 
+    #assert 
     assert_frame_equal(expected, result, check_dtype=False)
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+def add_product_price_colume(df_arg):
+    """
+    Add 'product_price' colume for product price
+    Rename column "products+price" to "products"
+    """
+    df_arg["product_price"] = df_arg["products+price"].str.split(" - ").str[-1]
+    df_arg = remove_price_from_products(df_arg)
+    df_arg.rename(columns={'products+price':'products'}, inplace=True)
+    return df_arg
+
+def test_add_product_price_column():
+    #assemble
+    data = {'products+price':['coffee - 12', 'tea - 10', 'water - 8']}
+    df = pd.DataFrame(data=data)
+    mock_remove_price_from_product = Mock()
+    mock_pandas_rename = Mock()
+    data1 = {'product+price':['coffee', 'tea', 'water']}
+    df1 = pd.DataFrame(data=data1)
+    mock_remove_price_from_product.return_value = df1
+    mock_pandas_rename.return_value = pd.DataFrame({'products': ['coffee', 'tea', 'water'], 'product_price':['12', '10', '8']})
+    expected = mock_pandas_rename()
+    #act
+    result = add_product_price_colume(df)
+    #assert
+    assert_frame_equal(expected, result)
+    pass
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+def remove_price_from_products(df_arg):
+    df_arg["products+price"] = df_arg["products+price"].map(lambda x:x.rstrip(' -0123456789.'))
+    return df_arg
+
+def test_remove_price_from_products():
+    #assemble 
+    data = {'products+price':['coffee - 12', 'tea - 10', 'water - 8']}
+    df = pd.DataFrame(data=data)
+    mock_map_lambda_func = Mock()
+    mock_map_lambda_func.return_value = pd.DataFrame({'products+price': ['coffee', 'tea', 'water']})
+    expected = mock_map_lambda_func()
+    #act
+    result = remove_price_from_products(df)
+    #assert
+    assert_frame_equal(expected, result)
+
+    
 
