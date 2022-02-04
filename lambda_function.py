dff712f8da1b97662e324fc780cbf94dc774cd93
victorchu620 <waiyat620@gmail.com>
2022-02-04 16:10:02 +0000

    lambda_function.py: added some python code

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,7 @@ import logging
 import boto3
 import pandas as pd
 import os
+import hashlib
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
@@ -19,10 +20,70 @@ def handler(event, context):
     s3.meta.client.download_file(bucket, key, f"/tmp/{filename}")
     # File downloaded to /tmp directory
     
-    # Use pandas to read the csv.
-    df = pd.read_csv(f"/tmp/{filename}")
+    # Use python-pandas to read and normalise the data from csv.
+    def create_hash_id(df_arg: pd.DataFrame):
+        """
+        Generate a hash id based on the original data (before removing any data)
+        hashing method still waiting to be updated
+        """
+        df_arg["id"]=df_arg.astype(str).sum(1).apply(lambda x:hashlib.md5(x.encode()).hexdigest())
+        return df_arg
 
-    # Log the dataframe head.
-    print(df.head())
+    def drop_column(df_arg: pd.DataFrame, column:str):
+        "Drop any column in the DataFrame"
+        df_arg.drop(column, inplace=True, axis=1)
+        return df_arg
+        
+    def set_index(df_arg:pd.DataFrame, column:str):
+        "Set specific column as index"
+        df_arg.set_index(column, inplace = True)
+        return df_arg
+
+    def products_price_explode(df_arg: pd.DataFrame, column:str, split_criteria:str):
+        "Perform 1NF explode to products+price where each role contain one product only"
+        df_func = df_arg.copy()
+        df_func[column] = df_func[column].map(str)
+        df_func[column] = df_func[column].str.split(split_criteria)
+        df_func = df_func.explode(column)
+        print(df_func)
+        return df_func
+
+    def add_product_price_colume(df_arg: pd.DataFrame):
+        """
+        Add 'product_price' colume for product price
+        Rename column "products+price" to "products"
+        """
+        df_arg["product_price"] = df_arg["products+price"].str.split(" - ").str[-1]
+        df_arg = remove_price_from_products(df_arg)
+        df_arg.rename(columns={'products+price':'products'}, inplace=True)
+        return df_arg
+
+    def remove_price_from_products(df_arg):
+        "data cleansing: remove price from product"
+        df_arg["products+price"] = df_arg["products+price"].map(lambda x:x.rstrip(' -0123456789.'))
+        return df_arg
+
+    def load_csv_to_df(path:str):
+        "load csv file to python in pandas DataFrame format"
+        df = pd.read_csv(path, names = ["datetime","Location","fullname", "products+price", "total_price","payment_type","card_number"])
+        return df
+
+    def copy_of_original_data(df_arg):
+        df_copy = df_arg.copy()
+        return df_copy
+
+    #------------------------------------------------------------------------
+    # Load csv into python as pandas DataFrame
+    df_original = load_csv_to_df(f"/tmp/{filename}")
+
+    # Perform data_normalization
+    df_transformed = copy_of_original_data(df_original)
+    df_transformed = create_hash_id(df_transformed)
+    df_transformed = products_price_explode(df_transformed, "products+price", ",")
+    df_transformed = add_product_price_colume(df_transformed)
+    df_transformed = drop_column(df_transformed, "card_number")
+    df_transformed = drop_column(df_transformed, "fullname")
+    df_transformed = set_index(df_transformed, "id")
+    print(df_transformed)
     
     print("Ex4 func complete")
