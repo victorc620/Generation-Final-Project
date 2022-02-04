ddd1c2a0a3d80441057c41451352af57bf859a85
Angela <angelatjesus@gmail.com>
2022-02-04 11:42:54 +0000

    Update SQL Script

--- cafe_intel.sql
+++ cafe_intel.sql
@@ -5,30 +5,27 @@ DROP SEQUENCE IF EXISTS cafe_id_cafe_seq;
 CREATE SEQUENCE cafe_id_cafe_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 3 CACHE 1;
 
 CREATE TABLE "public"."cafe" (
-    "id_cafe" integer DEFAULT nextval('cafe_id_cafe_seq') NOT NULL,
-    "name_cafe" character varying(45),
-    CONSTRAINT "cafe_pkey" PRIMARY KEY ("id_cafe")
+    "cafe_id" integer DEFAULT nextval('cafe_id_cafe_seq') NOT NULL,
+    "location" character varying(45),
+    CONSTRAINT "cafe_pkey" PRIMARY KEY ("cafe_id")
 ) WITH (oids = false);
 
 
 DROP TABLE IF EXISTS "orders";
-DROP SEQUENCE IF EXISTS orders_id_order_seq;
-CREATE SEQUENCE orders_id_order_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
 CREATE TABLE "public"."orders" (
-    "id_order" integer DEFAULT nextval('orders_id_order_seq') NOT NULL,
-    "id_cafe" integer,
+    "order_id" character varying(45) NOT NULL,
+    "cafe_id" integer,
     "date" timestamp,
     "payment_type" character varying(45),
     "total_price" real,
-    CONSTRAINT "orders_pkey" PRIMARY KEY ("id_order")
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("order_id")
 ) WITH (oids = false);
 
 
 DROP TABLE IF EXISTS "orders_products";
 CREATE TABLE "public"."orders_products" (
-    "id_order" integer,
-    "id_product" integer,
+    "order_id" character varying(45) NOT NULL,
+    "product_id" integer,
     "quantity_purchased" integer
 ) WITH (oids = false);
 
@@ -38,17 +35,16 @@ DROP SEQUENCE IF EXISTS products_products_id_seq;
 CREATE SEQUENCE products_products_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
 CREATE TABLE "public"."products" (
-    "id_product" integer DEFAULT nextval('products_products_id_seq') NOT NULL,
+    "product_id" integer DEFAULT nextval('products_products_id_seq') NOT NULL,
     "name" character varying(45),
-    "size" character varying(45),
     "price" double precision,
-    CONSTRAINT "products_pkey" PRIMARY KEY ("id_product")
+    CONSTRAINT "products_pkey" PRIMARY KEY ("product_id")
 ) WITH (oids = false);
 
 
-ALTER TABLE ONLY "public"."orders" ADD CONSTRAINT "id_cafe" FOREIGN KEY (id_cafe) REFERENCES cafe(id_cafe) NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."orders" ADD CONSTRAINT "id_cafe" FOREIGN KEY (cafe_id) REFERENCES cafe(cafe_id) NOT DEFERRABLE;
 
-ALTER TABLE ONLY "public"."orders_products" ADD CONSTRAINT "id_order" FOREIGN KEY (id_order) REFERENCES orders(id_order) NOT DEFERRABLE;
-ALTER TABLE ONLY "public"."orders_products" ADD CONSTRAINT "id_product" FOREIGN KEY (id_product) REFERENCES products(id_product) NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."orders_products" ADD CONSTRAINT "id_product" FOREIGN KEY (product_id) REFERENCES products(product_id) NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."orders_products" ADD CONSTRAINT "orders_products_id_order_fkey" FOREIGN KEY (order_id) REFERENCES orders(order_id) NOT DEFERRABLE;
 
--- 2022-01-28 14:09:22.877378+00
+-- 2022-02-04 11:33:41.534847+00
