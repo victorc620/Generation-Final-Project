a7a523ee18fc9a53a443dc7b06b4d505bfd345b6
victorchu620 <waiyat620@gmail.com>
2022-02-09 06:24:37 +0000

    delete unwanted file in src file

--- src/test.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('/home/jean/Desktop/Cafe_Intel/team-4-project/src/chesterfield_25-08-2021_09-00-00.csv', names = ["datetime","Location","fullname", "products+price", "total_price","payment_type","card_number"])
-print(df.drop("card_number", inplace=True, axis=1))
