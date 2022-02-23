50077a2f4855e1a993516c2da37a71ce72c67e5b
victorchu620 <waiyat620@gmail.com>
2022-02-23 13:27:00 +0000

    cicd test

--- lambda_main_load_to_db.py
+++ lambda_main_load_to_db.py
@@ -32,16 +32,16 @@ def handler(event, context):
     if file_type == "csv":
         #insert into Red Shift
         if filename_without_time == "product_df":
-            insert_value('products', filename)
+            insert_value('products', filename, s3_bucket_name)
         
         elif filename_without_time == "location_df":
-            insert_value("cafe", filename)
+            insert_value("cafe", filename, s3_bucket_name)
         
         elif filename_without_time == "orders_df":
-            insert_value("orders", filename)
+            insert_value("orders", filename, s3_bucket_name)
         
         elif filename_without_time == "orders_products_df":
-            insert_value("orders_products", filename)
+            insert_value("orders_products", filename, s3_bucket_name)
     
     else:
         print(f"file type is {file_type} instead of csv, nothing loaded to redshift.")
